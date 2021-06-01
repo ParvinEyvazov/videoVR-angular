b7fb850d69d07156eda8f154a44b9eefe929114e
ParvinEyvazov <parvineyvazov@teknodev.biz>
2021-06-01 16:49:33 +0300

    new features added

--- src/app/pages/video-view/video-view.component.ts
+++ src/app/pages/video-view/video-view.component.ts
@@ -47,16 +47,41 @@ export class VideoViewComponent implements OnInit, AfterViewInit {
     this.player = videojs(this.element?.nativeElement, this.config, () => {
       console.log('Using video.js ' + videojs.VERSION);
       this.player.src({
-        src: 'http://img-ys011.didistatic.com/static/didiglobal/do1_pcUZZjSG7vFlMbdr8fA6#.mp4',
+        // src: 'https://storage.googleapis.com/download/storage/v1/b/hq-proje1-5b43f/o/6095b7c2db533d002cf66b09?alt=media&timestamp=1622535303368',
+        src: 'https://storage.googleapis.com/download/storage/v1/b/hq-proje1-5b43f/o/60b616bc21d2b2002daefc58?alt=media&timestamp=1622546139654',
+        // src: 'https://github.com/videojs/videojs-vr/blob/153c690f4141c3a8d7e9399a6019a459b093c318/samples/eagle-360.mp4?raw=true',
+
+        // src: this.url,
         type: 'video/mp4',
       });
-      this.player.vr({ projection: '360' });
+    });
+
+    // this.player.vr({ projection: '360' }).cameraVector;
+    this.player.mediainfo = this.player.mediainfo || {};
+    // this.player.mediainfo.projection = '360_TB';
+
+    this.player.vr({
+      projection: '360',
+      // debug: true,
+      forceCardboard: false,
     });
 
     // error handling
     this.player.on('error', (error: any) => {
       console.warn(error);
     });
+
+    // setTimeout(() => {
+    //   this.player.pause(); // pause video
+    //   this.player.currentTime(100); // get this time
+    // }, 6000);
+
+    // setTimeout(() => {
+    //   var whereYouAt = this.player.currentTime(); // get current time
+    //   console.log(whereYouAt);
+    //   this.player.controls(false); // hide control
+    //   this.player.play(); // play video
+    // }, 8000);
   }
 
   ngOnInit() {
@@ -67,5 +92,25 @@ export class VideoViewComponent implements OnInit, AfterViewInit {
     // let a: string = this.route.snapshot.paramMap.get('url') || '';
     // this.url = decodeURIComponent(a);
     // console.log(decodeURIComponent(a));
+  }
+
+  play() {
+    this.player.play();
+  }
+
+  pause() {
+    this.player.pause();
+  }
+
+  go(time: number) {
+    this.player.currentTime(time);
+  }
+
+  hideControl() {
+    this.player.controls(false);
+  }
+
+  showControl() {
+    this.player.controls(true);
   }
 }
